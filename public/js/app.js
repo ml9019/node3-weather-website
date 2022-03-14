3f2e398032d77e22994c912fda73c9d673ec9679
Mateusz Lukasiewicz <Mateusz.Lukasiewicz@keoic.com>
2022-03-14 18:16:35 +0400

    For Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -14,16 +14,15 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch("http://localhost:3000/weather?address=" + location).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error;
-        } else {
-          messageOne.textContent = "Location: " + data.location;
-          messageTwo.textContent = "Temperature: " + data.forecast + " degrees";
-        }
-      });
-    }
-  );
+  //  fetch("http://localhost:3000/weather?address=" + location).then(
+  fetch("/weather?address=" + location).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error;
+      } else {
+        messageOne.textContent = "Location: " + data.location;
+        messageTwo.textContent = "Temperature: " + data.forecast + " degrees";
+      }
+    });
+  });
 });
